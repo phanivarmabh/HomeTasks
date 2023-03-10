7c8054e8baf9f08d7233de7bd3ef8ad7314427de
phanivarmabh <41978669+phanivarmabh@users.noreply.github.com>
2023-03-10 09:14:45 +0530

    Update Home_Task_2.sql

--- Home_Task_2.sql
+++ Home_Task_2.sql
@@ -16,6 +16,9 @@ begin
 		+ ' select ''' + TABLE_CATALOG + ''' as Database_Name,''' + TABLE_SCHEMA + ''' as Table_Schema,''' + TABLE_NAME + ''' as Table_Name,
 		''' + Column_Name + ''' as Column_Name,''' + DATA_TYPE + ''' as Data_Type, count(distinct ' + quotename (Column_Name) + ' ) as DistinctCountValue, 
 		count( '+ quotename (Column_Name) + ') as TotalRowCount, sum(case when '+ quotename (Column_Name) + ' is NULL then 1 else 0 END) as NullValueCount,
+		sum(case when ASCII( '+ quotename (Column_Name) + ') between 65 and 90 then 1 else 0 end) as UpperCaseCount,
+		sum(case when ASCII( '+ quotename (Column_Name) + ') between 97 and 122 then 1 else 0 end) as LowerCaseCount,
+		SUM(CASE WHEN (( ASCII('+ quotename (Column_Name) + ') NOT BETWEEN 97 AND 122 ) AND ( ASCII('+ quotename (Column_Name) + ') NOT BETWEEN 65 AND 90 )) THEN 1 ELSE 0 END) as OtherCharCount,
 		min(cast('+ quotename (Column_Name) + ' as varchar)) minValue, max(cast('+ quotename (Column_Name) + ' as varchar)) maxValue
 		FROM ' + quotename (Table_Schema) + '.' + quotename (Table_Name)
 	from cols
